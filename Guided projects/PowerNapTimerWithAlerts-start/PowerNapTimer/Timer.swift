9dc7348b0967ece1d96433a4c134b9e6e3ebf53b
kpfister <kpfister@me.com>
2016-05-17 10:53:23 -0600

    Comments

--- Guided projects/PowerNapTimerWithAlerts-start/PowerNapTimer/Timer.swift
+++ Guided projects/PowerNapTimerWithAlerts-start/PowerNapTimer/Timer.swift
@@ -10,31 +10,32 @@ import UIKit
 
 class Timer: NSObject {
     
-    var timeRemaining: NSTimeInterval?
+    var timeRemaining: NSTimeInterval?// This is optional so it doesnt start unless the timer is started.
     
-    var isOn: Bool {
+    var isOn: Bool { // This is a computed property
         if timeRemaining != nil {
-            return true
+            return true // if time remaining is not equal to zer0 - its on
         } else {
             return false
         }
     }
     
     func timeAsString() -> String {
-        let timeRemaining = Int(self.timeRemaining ?? 20*60)
-        let minutesLeft = timeRemaining / 60
-        let secondsLeft = timeRemaining - (minutesLeft*60)
+        let timeRemaining = Int(self.timeRemaining ?? 20*60) // ?? is nil coelsent. 20*60 is 20 mins.
+        let minutesLeft = timeRemaining / 60 //timeRemaining is in secs, so we devide by 60 to see min
+        let secondsLeft = timeRemaining - (minutesLeft*60) //This allows us to calulate the number of seonds
         return String(format: "%02d : %02d", arguments: [minutesLeft, secondsLeft])
-    }
+    }      // ^ this String is formating the clock to show our values.
 
-    @objc private func secondTick() {
-        guard let timeRemaining = timeRemaining else {return}
-        if timeRemaining > 0 {
-            self.timeRemaining = timeRemaining - 1
+    @objc private func secondTick() { // everySecond this method is called
+        guard let timeRemaining = timeRemaining else {return} // checks if there is time remaining
+        if timeRemaining > 0 { // if timer is greater than zero then we can decrease the timer
+            self.timeRemaining = timeRemaining - 1 // decreaseing the timer by 1 sec
             performSelector(#selector(Timer.secondTick), withObject: nil, afterDelay: 1)
+            // ^ this is from Objective C. Selector is where we call this method
             NSNotificationCenter.defaultCenter().postNotificationName("secondTick", object: nil)
         } else {
-            self.timeRemaining = nil
+            self.timeRemaining = nil // If the timer is 0 set the time remaining to Nil
             NSNotificationCenter.defaultCenter().postNotificationName("timerCompleted", object: nil)
         }
     }
